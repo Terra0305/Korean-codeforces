5d42740df6292401dfd4496a9eb31c3555797cb4
kimbabZIP <02legend.park@gmail.com>
2025-12-28 15:39:08 +0900

    feat: axios 의존성 추가

---
+++ frontend/app/src/types/auth.d.tsx
@@ -0,0 +1,25 @@
+export interface Profile {
+    school: string;
+    department: string;
+    student_id: string;
+    real_name: string;
+    codeforces_id: string;
+    elo_rating: number;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface User {
+    id: number;
+    name: string;
+    profile: Profile;
+}
+
+export interface LoginResponse {
+    message: string;
+    user: User;
+}
+
+export interface ApiError {
+    error: string;
+}
