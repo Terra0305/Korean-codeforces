b3d240920e6cb7c1c8c0b9c6dc34684d0e36b454
PARK SUNGMIN <jpsm0305@naver.com>
2026-01-03 15:39:07 +0900

    fix: whitelisting azure static web app for cors/csrf

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -160,6 +160,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "https://frontend-28mi.onrender.com",  # Render Frontend
+    "https://mango-mud-0cd127600.6.azurestaticapps.net",  # Azure Static Web App
 ]
 
 if os.environ.get("CORS_ALLOWED_ORIGINS"):
@@ -196,6 +197,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "https://frontend-28mi.onrender.com",  # Render Frontend
+    "https://mango-mud-0cd127600.6.azurestaticapps.net",  # Azure Static Web App
 ]
 
 if os.environ.get("CSRF_TRUSTED_ORIGINS"):
