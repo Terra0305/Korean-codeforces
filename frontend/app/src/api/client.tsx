d7e6dd7c9ce5e28503007a6ce2331ba4aa6a2a5d
kimbabZIP <02legend.park@gmail.com>
2026-01-22 23:39:56 +0900

    fix: 라우팅 할 때 admin은 지움(nginx에서 django로 계속 get 보내서)

--- frontend/app/src/api/client.tsx
+++ frontend/app/src/api/client.tsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
 
 const client = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -9,5 +12,16 @@ const client = axios.create({
         "Content-Type": "application/json",
     },
 });
+
+client.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status === 401) {
+            alert("세션이 만료되었습니다.");
+            navigate('/login');
+        }
+        return Promise.reject(error);
+    }
+)
 
 export default client;
